82666519b490406a74ab34acb7f2d6fe0c249dfc
Brandon Felty <brcfelty@gmail.com>
2022-06-08 16:14:12 -0600

    adds dashboard component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,14 @@
 import useLocalStorage from '../hooks/useLocalStorage';
 import '../styles/App.css';
 import Login from './Login';
+import Dashboard from './Dashboard'
 
 function App() {
   const [id, setId] = useLocalStorage('username')
 
   return (
     <>
-      {id ? <h1>hello</h1>: <Login onIdSubmit={setId} />}
+      {id ? <Dashboard id={id}/>: <Login onIdSubmit={setId} />}
     </>
   );
 }
