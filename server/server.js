4293aa565dea2938f3b566c67f1e0a407a87cafa
Brandon Felty <brcfelty@gmail.com>
2022-06-10 18:47:45 -0600

    connects to mongodb atlas

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+require('dotenv').config()
+
+USERNAME = process.env.USERNAME
+PASSWORD = process.env.PASSWORD
+
 const io = require('socket.io')(4321, {
   cors: {
     origin: "http://localhost:3000",
@@ -5,17 +10,48 @@ const io = require('socket.io')(4321, {
   }
 })
 
-io.on('connection', socket => {
-const id = socket.handshake.query.id
-socket.join(id)
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@chatter.hvjqadm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const users = client.db("chatter").collection('users')
+  console.log("err:", err)
 
-socket.on('send-message', ({ recipients, text }) => {
-  recipients.forEach(recipient => {
-    const newRecipients = recipients.filter(r => r !== recipient)
-    newRecipients.push(id)
-    socket.broadcast.to(recipient).emit('receive-message', {
-      recipients: newRecipients, sender: id, text
+
+  users.find().forEach(function(item){
+    console.log(item)
+    })
+
+
+  // const findResult = users.find({}, ()=> {}).toArray();
+  // console.log('Found documents =>', findResult);
+
+  // console.log(users)
+  // users.findOne({}, function(err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+    
+  // });
+
+  io.on('connection', socket => {
+    const id = socket.handshake.query.id
+    socket.join(id)
+    
+    socket.on('send-message', ({ recipients, text }) => {
+      recipients.forEach(recipient => {
+        const newRecipients = recipients.filter(r => r !== recipient)
+        newRecipients.push(id)
+        socket.broadcast.to(recipient).emit('receive-message', {
+          recipients: newRecipients, sender: id, text
+        })
+      })
     })
   })
-})
-})
+  
+  
+  // client.close();
+});
+
+
+
+
