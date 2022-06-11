387623cbbd60affa18571aaa1f8dc4b5e1e4da8c
Brandon Felty <brcfelty@gmail.com>
2022-06-10 18:49:54 -0600

    socket io error

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './styles/index.css';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
